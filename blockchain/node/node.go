b1fcc26aba6b9ec69f535e114ac059aba470cd28
Mike Christensen <mike_christensen@btinternet.com>
2017-11-16 19:49:22 +0000

    read network config from file and start nodes concurrently

--- blockchain/node/node.go
+++ blockchain/node/node.go
@@ -1,20 +1,90 @@
 package node
 
-type node struct {
+import (
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/mschristensen/brocoin/blockchain/api"
+	"github.com/mschristensen/brocoin/blockchain/core"
+)
+
+type Node struct {
 	Address string
 	Peers   []string
+	Chain   core.Blockchain
 }
 
-var self node
+func (node *Node) Start(wg *sync.WaitGroup) {
 
-func GetSelf() node {
-	return self
-}
+	fmt.Println("Started Node on " + node.Address)
 
-func Init(address string, peers []string) node {
-	self := node{
-		Address: address,
-		Peers:   peers,
+	// set up channels to communicate with goroutines
+	minerChanT := make(chan core.Transaction) // to forward inbound transactions to miner
+	minerChanB := make(chan core.Block)       // to receive mined blocks from miner
+	minerChanLB := make(chan core.Block)      // to send updated last block on chain to miner
+	apiChanT := make(chan core.Transaction)   // to receive inbound transactions from api
+	apiChanB := make(chan core.Block)         // to receive inbound blocks from api
+
+	// handle incoming requests from peer nodes, streaming out the data along the channels
+	go api.Listen(node.Address, apiChanT, apiChanB)
+
+	// listen for incoming transactions received from peer nodes and forward to the mining process
+	go receiveTransactions(apiChanT, minerChanT)
+
+	// mine blocks
+	go core.Mine(minerChanLB, minerChanT, minerChanB)
+
+	// set miner up with the initial last block
+	lb := node.Chain.LastBlock()
+	minerChanLB <- lb
+
+	var bMine core.Block
+	var bPeer core.Block
+	for {
+		select {
+		case bMine = <-minerChanB: // we've just mined a block
+			last := node.Chain.LastBlock()
+			if bMine.Validate(last) { // validate it
+				node.Chain.AddBlock(bMine) // add it to the chain
+
+				// broadcast the block to the network
+				r, err := api.Post("/block?peers="+strings.Join(node.Peers, ","), bMine.ToJSON())
+				if err != nil {
+					log.Fatal("Request to API resulted in an error")
+					panic(err)
+				}
+				if r.StatusCode != 200 {
+					log.Fatal(fmt.Sprintf("Request to API did not succeed, got HTTP %d", r.StatusCode))
+				}
+				m, err := api.ParseBody(r.Body)
+				if err != nil {
+					log.Fatal("API response body could not be parsed")
+					panic(err)
+				}
+				response := api.Response{}
+				err = response.FromMap(m)
+				if err != nil {
+					log.Fatal("API response body could not be written to Response object")
+					panic(err)
+				}
+
+			} else {
+				// TODO: Notify of rejected/invalid block
+				fmt.Println("Mined block rejected as invalid", bMine)
+			}
+		case bPeer = <-apiChanB: // received a block from a peer
+			fmt.Println("RECEIVED FROM PEER", bPeer)
+		}
 	}
-	return self
+}
+
+func receiveTransactions(apiChanT chan core.Transaction, minerChanT chan core.Transaction) {
+	for {
+		select {
+		case t := <-apiChanT:
+			minerChanT <- t
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,89 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"sync"
 
-	"github.com/mschristensen/brocoin/blockchain/api"
 	"github.com/mschristensen/brocoin/blockchain/core"
 	"github.com/mschristensen/brocoin/blockchain/node"
 )
 
 func main() {
-	// init this node
-	args := os.Args[1:]
-	self := node.Init(args[0], args[1:])
+	// read network configuration from file
+	inFile, _ := os.Open("network.config")
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
 
-	// init the blockchain
-	bc := core.NewBlockchain()
-
-	// set up channels to communicate with goroutines
-	minerChanT := make(chan core.Transaction) // to forward inbound transactions to miner
-	minerChanB := make(chan core.Block)       // to receive mined blocks from miner
-	minerChanLB := make(chan core.Block)      // to send updated last block on chain to miner
-	apiChanT := make(chan core.Transaction)   // to receive inbound transactions from api
-	apiChanB := make(chan core.Block)         // to receive inbound blocks from api
-
-	// handle incoming requests from peer nodes, streaming out the data along the channels
-	go api.Listen(self.Address, apiChanT, apiChanB)
-
-	// listen for incoming transactions received from peer nodes and forward to the mining process
-	go receiveTransactions(apiChanT, minerChanT)
-
-	// mine blocks
-	go core.Mine(minerChanLB, minerChanT, minerChanB)
-
-	// set miner up with the initial last block
-	lb := bc.LastBlock()
-	minerChanLB <- lb
-
-	var bMine core.Block
-	var bPeer core.Block
-	for {
-		select {
-		case bMine = <-minerChanB: // we've just mined a block
-			last := bc.LastBlock()
-			if bMine.Validate(last) { // validate it
-				bc.AddBlock(bMine) // add it to the chain
-
-				// broadcast the block to the network
-				r, err := api.Post("/block?peers="+strings.Join(self.Peers, ","), bMine.ToJSON())
-				if err != nil {
-					log.Fatal("Request to API resulted in an error")
-					panic(err)
-				}
-				if r.StatusCode != 200 {
-					log.Fatal(fmt.Sprintf("Request to API did not succeed, got HTTP %d", r.StatusCode))
-				}
-				m, err := api.ParseBody(r.Body)
-				if err != nil {
-					log.Fatal("API response body could not be parsed")
-					panic(err)
-				}
-				response := api.Response{}
-				err = response.FromMap(m)
-				if err != nil {
-					log.Fatal("API response body could not be written to Response object")
-					panic(err)
-				}
-
-			} else {
-				// TODO: Notify of rejected/invalid block
-				fmt.Println("Mined block rejected as invalid", bMine)
-			}
-		case bPeer = <-apiChanB: // received a block from a peer
-			fmt.Println("RECEIVED FROM PEER", bPeer)
-		}
+	// create the nodes
+	var nodes []node.Node
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		nodes = append(nodes, node.Node{
+			Address: parts[0],
+			Peers:   parts[1:],
+			Chain:   core.NewBlockchain(),
+		})
 	}
-}
 
-func receiveTransactions(apiChanT chan core.Transaction, minerChanT chan core.Transaction) {
-	for {
-		select {
-		case t := <-apiChanT:
-			minerChanT <- t
-		}
+	// start the nodes
+	var wg sync.WaitGroup
+	for i := 0; i < len(nodes); i++ {
+		fmt.Println(nodes[i].Address)
+		wg.Add(1)
+		go nodes[i].Start(&wg)
 	}
+
+	// Wait for all HTTP fetches to complete.
+	wg.Wait()
 }
